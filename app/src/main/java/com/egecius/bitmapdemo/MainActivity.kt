747bab086cdf53e754356ff1c2f2809cb5783dad
egis <e.gecius@gmail.com>
2021-05-07 07:41:39 +0100

    show images

--- app/src/main/java/com/egecius/bitmapdemo/MainActivity.kt
+++ app/src/main/java/com/egecius/bitmapdemo/MainActivity.kt
@@ -2,10 +2,21 @@ package com.egecius.bitmapdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showImages()
+    }
+
+    private fun showImages() {
+        val bitmapPdqHeader = readFileFromResources("image_pdq_header", javaClass).toBitmap()
+        val bitmapSharp = readFileFromResources("image_from_sharp", javaClass).toBitmap()
+
+        findViewById<ImageView>(R.id.image_1).setImageBitmap(bitmapPdqHeader)
+        findViewById<ImageView>(R.id.image_2).setImageBitmap(bitmapSharp)
     }
 }
